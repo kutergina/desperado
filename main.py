bf283cac1c6e9c87a2e585e42da9d44a8a4f0c2d
kutergina <lyubovkutergina@gmail.com>
2017-05-21 23:48:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ parser.add_argument('--source', required=True, help='Is it start?')
 parser.add_argument('--radius', type=int, default=2, help='Radius of action')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))

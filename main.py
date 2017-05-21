09fed911ad30b8938b4d041d55cf0519e0788891
kutergina <lyubovkutergina@gmail.com>
2017-05-21 23:43:14 +0400

    edit main 1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Process bla bla bla')
-parser.add_argument('--source', help='Is it start?')
-parser.add_argument('--radius', help='Radius of action')
+parser.add_argument('--source', required=True, help='Is it start?')
+parser.add_argument('--radius', type=int, default=2, help='Radius of action')
 
 args = parser.parse_args()
 print(args.accumulate(args.integers))

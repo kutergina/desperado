3885e1538401047ebfa2d5cf3377becb298abf0e
kutergina <lyubovkutergina@gmail.com>
2017-05-23 00:46:26 +0400

    23/05/2017 00:46

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ parser.add_argument('--source', required=True, help='Is it start?')
 parser.add_argument('--radius', type=int, default=2, help='Radius of action')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+
+import requests
+r = requests.get(args.source, stream=True, timeout=10)
+if r.headers['Content-Type'].find('text/html') != -1:
+    print(r.headers)
+else:
+    print ('This is not text!')
+
